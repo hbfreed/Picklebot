711878f978afd0a1af5206ecb1bdf2e5bf6db9f7
henry <hbfreed@protonmail.com>
2023-09-14 11:20:30 -0700

    changed header=None in read_csv

--- dataloader.py
+++ dataloader.py
@@ -27,7 +27,7 @@ def custom_collate(batch): #this custom collate pads our batch.
 
 class PicklebotDataset(Dataset):
     def __init__(self, annotations_file, video_dir, transform=None,target_transform=None):
-        self.video_labels = pd.read_csv(annotations_file)
+        self.video_labels = pd.read_csv(annotations_file,engine='pyarrow',header=None)
         self.video_dir = video_dir
         self.transform = transform
         self.target_transform = target_transform
