37090f3185cb6ba939fb6f3a6652938f35f7c455
henry <hbfreed@protonmail.com>
2023-09-27 12:46:40 -0700

    added dali to cloud_train, added helpers.py

---
+++ helpers.py
@@ -0,0 +1,7 @@
+import torch
+
+
+def calculate_accuracy(outputs,labels):
+    predicted_classes = torch.argmax(outputs,dim=1).to(labels.device)
+    num_correct = torch.sum(predicted_classes == labels).item()
+    return num_correct
