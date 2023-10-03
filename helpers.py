c8bb50d860ab235f9b4c3d03c3d94bacc4f1e143
henry <hbfreed@protonmail.com>
2023-10-02 20:42:29 -0700

    deleted init from helpers, is in mobilenet now

--- helpers.py
+++ helpers.py
@@ -10,19 +10,6 @@ def calculate_accuracy(outputs,labels):
     num_correct = torch.sum(predicted_classes == labels).item()
     return num_correct
 
-#initialize the model
-def initialize_mobilenet_weights(model):
-    for module in model.modules():
-        if isinstance(module, nn.Conv2d) or isinstance(module, nn.Conv3d):
-            # Initialize convolutional layers with appropriate initialization
-            init.kaiming_uniform_(module.weight, a=0, mode='fan_in', nonlinearity='relu')
-        elif isinstance(module, nn.BatchNorm2d) or isinstance(module, nn.BatchNorm3d):
-            # Initialize Batch Normalization layers with small values and biases to zero
-            init.constant_(module.weight, 1)
-            init.constant_(module.bias, 0)
-        elif isinstance(module, nn.Linear):
-            init.kaiming_uniform_(module.weight, a=0, mode='fan_in', nonlinearity='relu')
-
 #define our pipeline
 @pipeline_def
 def video_pipeline(file_root, sequence_length, initial_prefetch_size):
