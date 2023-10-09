36876ab09b7f4eb7fd088ab6edc07ad011f2c0ca
Henry <hbfreed@protonmail.com>
2023-10-09 14:32:17 -0700

    quick changes

--- helpers.py
+++ helpers.py
@@ -1,8 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.init as init
-# import nvidia.dali.fn as fn
-# from nvidia.dali import pipeline_def
+import nvidia.dali.fn as fn
+from nvidia.dali import pipeline_def
 
 #calculate the accuracy of the model, 
 def calculate_accuracy(outputs,labels):
@@ -21,10 +21,10 @@ def average_for_plotting(loss_list,window_size=1000):
     else:
         avg_losses = torch.tensor(loss_list).view(-1,1000).mean(1)
     return avg_losses
-# #define our pipeline
-# @pipeline_def
-# def video_pipeline(file_root, sequence_length, initial_prefetch_size):
-#     videos, labels = fn.readers.video(device="gpu", file_root=file_root, sequence_length=sequence_length,
-#                               shard_id=0, num_shards=1, random_shuffle=True, initial_fill=initial_prefetch_size,pad_sequences=True,
-#                               file_list_include_preceding_frame=False)
-#     return videos, labels
+#define our pipeline
+@pipeline_def
+def video_pipeline(file_root, sequence_length, initial_prefetch_size):
+    videos, labels = fn.readers.video(device="gpu", file_root=file_root, sequence_length=sequence_length,
+                              shard_id=0, num_shards=1, random_shuffle=True, initial_fill=initial_prefetch_size,pad_sequences=True,
+                              file_list_include_preceding_frame=False)
+    return videos, labels
