4c66f76a34dc892771d8ee978148edd7c521e862
Henry <hbfreed@protonmail.com>
2023-12-05 15:00:33 -0800

    workign on fixing movinet

--- movinet.py
+++ movinet.py
@@ -1,3 +1,5 @@
+import math
+import torch
 import torch.nn as nn
 from torch.nn import init
 from mobilenet import SEBlock3D
@@ -38,14 +40,19 @@ class MoviNetBottleneck(nn.Module):
         return x
 
 
-
 #A2 takes 224x224 resolution video as specified in the paper
 class MoViNetA2(nn.Module):
-    def __init__(self, num_classes=2):
+    def __init__(self, num_classes=2, subclip_length=50,buffer_size=2):
         super().__init__()
 
         #define our number of classes
         self.num_classes = num_classes
+
+        #initialize the subclip length
+        self.subclip_length=subclip_length
+
+        #initialize the stream buffers
+        self.buffer_size = buffer_size
 
         #define our first block, a 3D convolutional layer with 16 filters, kernel size of 1x3x3, stride of 1x2x2, and padding of 0x1x1  
         self.block1 = nn.Sequential(
@@ -61,7 +68,7 @@ class MoViNetA2(nn.Module):
             #2
             MoviNetBottleneck(in_channels=16, out_channels=16, expanded_channels=40, kernel_size=3),
             #3
-            MoviNetBottleneck(in_channels=16, out_channels=40, expanded_channels=96,kernel_size=3,dropout=0.2)
+            MoviNetBottleneck(in_channels=16, out_channels=40, expanded_channels=64,kernel_size=3,dropout=0.2)
         )
 
         self.block3 = nn.Sequential(
@@ -124,12 +131,11 @@ class MoViNetA2(nn.Module):
             nn.Conv3d(in_channels=640,out_channels=640,kernel_size=1,bias=False),
             nn.BatchNorm3d(640),
             nn.Hardswish(),
-            nn.Dropout3d(0.2)
+            nn.Dropout3d(0.2),
+            nn.AdaptiveAvgPool3d((1,1,1))
         )
 
         self.classifier = nn.Sequential(
-            nn.AdaptiveAvgPool3d((1,1,1)),
-            nn.Dropout3d(0.2),
             nn.Flatten(),
             nn.Linear(640, 2048),
             nn.BatchNorm1d(2048),
@@ -139,6 +145,61 @@ class MoViNetA2(nn.Module):
         )
             
 
+#I think, in this forward pass, we have implemented the stream buffer from the paper.
+    def forward(self, x):
+        batch_size, C, T, H, W = x.shape
+        num_clips = math.ceil(T/self.subclip_length)
+        
+        # Initialize buffers for each block
+        buffer1 = torch.zeros(batch_size, 3, self.buffer_size, H, W, device=x.device).requires_grad_(False)
+        buffer2 = torch.zeros(batch_size, 16, self.buffer_size, H//2, W//2, device=x.device).requires_grad_(False)
+        buffer3 = torch.zeros(batch_size, 40, self.buffer_size, H//4, W//4, device=x.device).requires_grad_(False)
+        buffer4 = torch.zeros(batch_size, 72, self.buffer_size, H//8, H//8, device=x.device).requires_grad_(False)
+        buffer5 = torch.zeros(batch_size, 72, self.buffer_size, H//8, H//8, device=x.device).requires_grad_(False)
+        buffer6 = torch.zeros(batch_size, 144, self.buffer_size, H//16, H//16, device=x.device).requires_grad_(False)
+        
+        pooled_outputs = []
+
+        for i in range(num_clips):
+            start_idx = i * self.subclip_length
+            end_idx = start_idx + self.subclip_length
+            x_clip = x[:, :, start_idx:end_idx, :, :]
+
+            # Block 1
+            out = self.block1(x_clip)
+            buffer1 = out[:, :, -self.buffer_size:, :, :].detach()
+            # Block 2
+            x_with_buffer = torch.cat((buffer1, out), dim=2)
+            out = self.block2(x_with_buffer)
+            buffer2 = out[:, :, -self.buffer_size:, :, :].detach()
+
+            x_with_buffer = torch.cat((buffer2, out), dim=2)
+            out = self.block3(x_with_buffer)
+            buffer3 = out[:, :, -self.buffer_size:, :, :].detach()
+
+            x_with_buffer = torch.cat((buffer3, out), dim=2)
+            out = self.block4(x_with_buffer)
+            buffer4 = out[:, :, -self.buffer_size:, :, :].detach()
+            
+            x_with_buffer = torch.cat((buffer4, out), dim=2)
+            out = self.block5(x_with_buffer)
+            buffer5 = out[:, :, -self.buffer_size:, :, :].detach()
+            
+            x_with_buffer = torch.cat((buffer5, out), dim=2)
+            out = self.block6(x_with_buffer)
+            buffer6 = out[:, :, -self.buffer_size:, :, :].detach()
+
+            
+            x_with_buffer = torch.cat((buffer6, out), dim=2)
+            out = self.conv(x_with_buffer)
+            
+            pooled_outputs.append(out)
+
+        out = torch.cat(pooled_outputs, dim=2)
+        out = nn.AdaptiveAvgPool3d((1,1,1))(out)
+        out = self.classifier(out)
+        return out
+    
     def forward(self,x):
         x = self.block1(x)
         x = self.block2(x)
@@ -150,14 +211,16 @@ class MoViNetA2(nn.Module):
         x = self.classifier(x)
         return x
 
-    def initialize_weights(self):
-        for module in self.modules():
-            if isinstance(module, nn.Conv3d) or isinstance(module, nn.Linear):
-                if hasattr(module, "nonlinearity"):
-                    if module.nonlinearity == 'relu':
-                        init.kaiming_normal_(module.weight, mode='fan_out', nonlinearity='relu')
-                    elif module.nonlinearity == 'hardswish':
-                        init.xavier_uniform_(module.weight)
-            elif isinstance(module, nn.BatchNorm3d) or isinstance(module, nn.BatchNorm1d):
-                init.constant_(module.weight, 1)
-                init.constant_(module.bias, 0)
+def initialize_weights(self):
+    for m in self.modules():
+        if isinstance(m, nn.Conv3d):
+            init.kaiming_uniform_(m.weight, mode='fan_in', nonlinearity='relu')
+            if m.bias is not None:
+                init.constant_(m.bias, 0)
+        elif isinstance(m, nn.BatchNorm3d):
+            init.constant_(m.weight, 1)
+            init.constant_(m.bias, 0)
+        elif isinstance(m, nn.Linear):
+            init.xavier_uniform_(m.weight)
+            if m.bias is not None:
+                init.constant_(m.bias, 0)
